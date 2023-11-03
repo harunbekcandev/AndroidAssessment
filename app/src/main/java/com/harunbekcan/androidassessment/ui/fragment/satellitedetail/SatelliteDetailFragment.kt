d833c11df836c38f69fd326f1da3702ae053ac6e
Harun Bekcan <harunbekcan@gmail.com>
2023-11-03 15:08:54 +0300

    SatelliteDetailFragment created.

---
+++ app/src/main/java/com/harunbekcan/androidassessment/ui/fragment/satellitedetail/SatelliteDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.harunbekcan.androidassessment.ui.fragment.satellitedetail
+
+import android.os.Bundle
+import com.harunbekcan.androidassessment.base.BaseFragment
+import com.harunbekcan.androidassessment.databinding.FragmentSatelliteDetailBinding
+
+class SatelliteDetailFragment : BaseFragment<FragmentSatelliteDetailBinding>(FragmentSatelliteDetailBinding::inflate) {
+    override fun prepareView(savedInstanceState: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+}
--- app/src/main/java/com/harunbekcan/androidassessment/ui/fragment/satellites/SatellitesFragment.kt
+++ app/src/main/java/com/harunbekcan/androidassessment/ui/fragment/satellites/SatellitesFragment.kt
@@ -1,11 +1,6 @@
-package com.harunbekcan.androidassessment.ui.satellites
+package com.harunbekcan.androidassessment.ui.fragment.satellites
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.harunbekcan.androidassessment.R
 import com.harunbekcan.androidassessment.base.BaseFragment
 import com.harunbekcan.androidassessment.databinding.FragmentSatellitesBinding
 
