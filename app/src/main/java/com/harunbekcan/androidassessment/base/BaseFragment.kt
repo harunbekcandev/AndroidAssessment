69f7b4805a272c1ca2c261d747387042783530ec
Harun Bekcan <harunbekcan@gmail.com>
2023-11-03 15:01:33 +0300

    BaseFragment and SatellitesFragment created.

---
+++ app/src/main/java/com/harunbekcan/androidassessment/base/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.harunbekcan.androidassessment.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<T : ViewBinding>(private val bindingInflater: (layoutInflater: LayoutInflater) -> T) : Fragment() {
+
+    private var _binding: T? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater.invoke(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        prepareView(savedInstanceState)
+    }
+
+    abstract fun prepareView(savedInstanceState: Bundle?)
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/harunbekcan/androidassessment/ui/satellites/SatellitesFragment.kt
@@ -0,0 +1,14 @@
+package com.harunbekcan.androidassessment.ui.satellites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.harunbekcan.androidassessment.R
+import com.harunbekcan.androidassessment.base.BaseFragment
+import com.harunbekcan.androidassessment.databinding.FragmentSatellitesBinding
+
+class SatellitesFragment : BaseFragment<FragmentSatellitesBinding>(FragmentSatellitesBinding::inflate) {
+    override fun prepareView(savedInstanceState: Bundle?) {}
+}
