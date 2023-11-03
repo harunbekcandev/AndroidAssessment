1016d7a03f619a5a899598d811075fa5d0e62cae
Harun Bekcan <harunbekcan@gmail.com>
2023-11-03 12:39:43 +0300

    build.gradle.kts updated.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.harunbekcan.androidassessment"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.harunbekcan.androidassessment"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
