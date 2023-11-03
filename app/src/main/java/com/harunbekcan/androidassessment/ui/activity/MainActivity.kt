95a9d3affc4b63b10c2cc11f9661f5099117bed0
Harun Bekcan <harunbekcan@gmail.com>
2023-11-03 15:27:15 +0300

    MyApplication created.

--- app/src/main/java/com/harunbekcan/androidassessment/ui/activity/MainActivity.kt
+++ app/src/main/java/com/harunbekcan/androidassessment/ui/activity/MainActivity.kt
@@ -3,10 +3,17 @@ package com.harunbekcan.androidassessment.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.harunbekcan.androidassessment.R
+import com.harunbekcan.androidassessment.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/harunbekcan/androidassessment/ui/fragment/satellites/SatellitesFragment.kt
+++ app/src/main/java/com/harunbekcan/androidassessment/ui/fragment/satellites/SatellitesFragment.kt
@@ -3,7 +3,9 @@ package com.harunbekcan.androidassessment.ui.fragment.satellites
 import android.os.Bundle
 import com.harunbekcan.androidassessment.base.BaseFragment
 import com.harunbekcan.androidassessment.databinding.FragmentSatellitesBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SatellitesFragment : BaseFragment<FragmentSatellitesBinding>(FragmentSatellitesBinding::inflate) {
     override fun prepareView(savedInstanceState: Bundle?) {}
 }
