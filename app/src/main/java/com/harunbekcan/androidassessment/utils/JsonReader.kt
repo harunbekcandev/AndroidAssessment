c2a801dea1fe20a3cc44abcf17896631837b452e
Harun Bekcan <harunbekcan@gmail.com>
2023-11-05 14:15:04 +0300

    JsonReader.kt created.

---
+++ app/src/main/java/com/harunbekcan/androidassessment/utils/JsonReader.kt
@@ -0,0 +1,14 @@
+package com.harunbekcan.androidassessment.utils
+
+import android.content.Context
+
+fun getJsonDataFromAsset(context: Context, fileName: String): String? {
+    val jsonString: String
+    try {
+        jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return null
+    }
+    return jsonString
+}
